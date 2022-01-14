0a26ed628a7f74a7067b8452bb2484e49a2dd76f
srdrbdrd <serdarbedir97@hotmail.com>
2022-01-14 23:31:31 +0300

    ItemCard finished

---
+++ src/components/ViewCartButton.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const ViewCartButton = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default ViewCartButton
+
+const styles = StyleSheet.create({})
